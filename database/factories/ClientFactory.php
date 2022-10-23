4916bb6585bb5285e54f23aaf0c665536df4648d
Guillermo García <guillermieres97@gmail.com>
2022-10-24 00:04:27 +0200

    seed db

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -9,7 +9,11 @@ class ClientFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => 'Client name',
+            'age' => 'Client age',
+            'city' => 'Client city',
+            'goal' => 'Client goal',
+            'medical history' => 'url'
         ];
     }
 }
