faa012321475bc83483afb6f59a6cdd6781d597d
Guillermo García <guillermieres97@gmail.com>
2022-10-24 00:13:41 +0200

    feat: ClientController list function and api route

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
     public function list() {
-        
+        $clients = Client::all();
+
+        return response()->json([
+            'status' => 1,
+            'msg' => 'Clients list',
+            'data' => $clients
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/clients/list', [ClientController::class, 'list'])->name('listTasks');
 
 
-
-Route::get('/clients/list', function() {
+Route::get('/clients/test', function() {
     return 'hello world';
 });
