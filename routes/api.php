0aee847c0bc9ace140a1e10cb0b7529881419a70
Guillermo García <guillermieres97@gmail.com>
2022-10-24 11:08:58 +0200

    feat: new fallback route

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::fallback(function() {
-    return '404 route not found';
-});
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -20,3 +16,7 @@ Route::post('/clients/create', [ClientController::class, 'create'])->name('creat
 // AuthController
 Route::post('/clients/register', [AuthController::class, 'register'])->name('register');
 
+// fallback route
+Route::any('{url?}/{sub_url?}', function() {
+    return '404 page not found';
+});
